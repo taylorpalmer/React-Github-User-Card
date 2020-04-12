b9ef881f20a08289130ad08682b04754541d3e90
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-04-11 21:03:19 -0700

    roughed out rendered data, needs styling

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -6,17 +6,29 @@ class App extends React.Component {
     super();
     this.state = {
       usersArr: [],
+      usersFollowers: [],
       usersText: "",
     };
   }
 
   componentDidMount() {
     console.log("componentDidMount");
+
     fetch(`https://api.github.com/users/taylorpalmer`)
       .then((res) => res.json())
       .then((users) => {
         console.log("Users: ", users);
         this.setState({ usersArr: users });
+      })
+      .catch((err) => {
+        console.log("Err: ", err);
+      });
+
+    fetch(`https://api.github.com/users/taylorpalmer/followers`)
+      .then((res) => res.json())
+      .then((followers) => {
+        console.log("Followers: ", followers);
+        this.setState({ usersFollowers: followers });
       })
       .catch((err) => {
         console.log("Err: ", err);
@@ -28,16 +40,29 @@ class App extends React.Component {
   }
 
   handleChanges = (e) => {
+    e.preventDefault();
     this.setState({ usersText: e.target.value });
+  };
+
+  submitHandler = (e) => {
+    e.preventDefault();
   };
 
   fetchUser = (e) => {
     e.preventDefault();
+    console.log("fetch");
 
     fetch(`https://api.github.com/users/${this.state.usersText}`)
       .then((res) => res.json())
       .then((users) => this.setState({ usersArr: users }))
       .catch((err) => console.log("Err: ", err));
+
+    fetch(`https://api.github.com/users/${this.state.usersText}/followers`)
+      .then((res) => res.json())
+      .then((followers) => {
+        console.log("Followers: ", followers);
+        this.setState({ usersFollowers: followers });
+      });
   };
 
   render() {
@@ -47,16 +72,35 @@ class App extends React.Component {
     console.log("userState: ", userState);
     return (
       <div>
-        <h1>Github Users</h1>
-        <input
-          type="text"
-          value={this.state.usersText}
-          onChange={this.handleChanges}
-        />
-        <button onClick={this.fetchUser}>Search</button>
+        <div>
+          <h1>Github Users</h1>
+          <input
+            type="text"
+            value={this.state.usersText}
+            onChange={this.handleChanges}
+          />
+          <button onClick={this.fetchUser}>Search</button>
+        </div>
+        <hr />
         <div className="users">
           <img src={userState.avatar_url} alt={userState.avatar_url} />
-          <p>{userState.name}</p>
+          <div className="name">
+            <h2>{userState.name}</h2>
+            <h3>Username: {userState.login}</h3>
+          </div>
+          <div className="urls">
+            <a href={userState.url}>Github Profile</a>
+            <br />
+            <a href={userState.repos_url}>Github Projects</a>
+          </div>
+          <div className="followers">
+            <a href={userState.followers_url}>Followers</a>
+            <p>Followers: {userState.followers}</p>
+          </div>
+          <div className="following">
+            <a href={userState.following_url}>Following</a>
+            <p>Following: {userState.following}</p>
+          </div>
         </div>
       </div>
     );
